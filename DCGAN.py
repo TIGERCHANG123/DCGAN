cfbf32d030b825b1fc8f45fa6146877c92a76c06
TIGERCHANG123 <2462924814@qq.com>
2020-02-22 12:08:54 +0800

    DCGAN

--- DCGAN.py
+++ DCGAN.py
@@ -7,12 +7,16 @@ class generator(tf.keras.Model):
     self.noise_shape = noise_shape
 
     self.model = tf.keras.Sequential()
-    self.model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=noise_shape))
+    self.model.add(layers.Dense(7 * 7 * 512, use_bias=False, input_shape=noise_shape))
     self.model.add(layers.BatchNormalization(momentum=0.9))
     self.model.add(tf.keras.layers.ReLU())
-    self.model.add(layers.Reshape((7, 7, 256)))
+    self.model.add(layers.Reshape((7, 7, 512)))
 
-    self.model.add(layers.Conv2DTranspose(128, (5, 5), strides=1, padding='same', use_bias=False))
+    self.model.add(layers.Conv2DTranspose(256, (5, 5), strides=1, padding='same', use_bias=False))
+    self.model.add(layers.BatchNormalization(momentum=0.9))
+    self.model.add(tf.keras.layers.ReLU())
+
+    self.model.add(layers.Conv2DTranspose(128, (5, 5), strides=2, padding='same', use_bias=False))
     self.model.add(layers.BatchNormalization(momentum=0.9))
     self.model.add(tf.keras.layers.ReLU())
 
@@ -20,11 +24,7 @@ class generator(tf.keras.Model):
     self.model.add(layers.BatchNormalization(momentum=0.9))
     self.model.add(tf.keras.layers.ReLU())
 
-    # self.model.add(layers.Conv2DTranspose(64, (5, 5), strides=2, padding='same', use_bias=False))
-    # self.model.add(layers.BatchNormalization(momentum=0.9))
-    # self.model.add(tf.keras.layers.ReLU())
-
-    self.model.add(layers.Conv2DTranspose(1, (5, 5), strides=2, padding='same', use_bias=False))
+    self.model.add(layers.Conv2DTranspose(1, (5, 5), strides=1, padding='same', use_bias=False))
     self.model.add(layers.Activation(activation='tanh'))
   def call(self, x):
     return self.model(x)
@@ -45,15 +45,15 @@ class discriminator(tf.keras.Model):
     self.model.add(tf.keras.layers.LeakyReLU(alpha=0.2))
     self.model.add(tf.keras.layers.Dropout(0.3))
 
-    self.model.add(tf.keras.layers.Conv2D(256, kernel_size=5, strides=2, padding="same"))
+    self.model.add(tf.keras.layers.Conv2D(256, kernel_size=5, strides=1, padding="same"))
     self.model.add(tf.keras.layers.BatchNormalization(momentum=0.9))
     self.model.add(tf.keras.layers.LeakyReLU(alpha=0.2))
     self.model.add(tf.keras.layers.Dropout(0.3))
 
-    # self.model.add(tf.keras.layers.Conv2D(512, kernel_size=5, strides=2, padding="same"))
-    # self.model.add(tf.keras.layers.BatchNormalization(momentum=0.9))
-    # self.model.add(tf.keras.layers.LeakyReLU(alpha=0.2))
-    # self.model.add(tf.keras.layers.Dropout(0.3))
+    self.model.add(tf.keras.layers.Conv2D(512, kernel_size=5, strides=1, padding="same"))
+    self.model.add(tf.keras.layers.BatchNormalization(momentum=0.9))
+    self.model.add(tf.keras.layers.LeakyReLU(alpha=0.2))
+    self.model.add(tf.keras.layers.Dropout(0.3))
 
     self.model.add(tf.keras.layers.Conv2D(256, kernel_size=1, strides=1, padding="same"))
 
