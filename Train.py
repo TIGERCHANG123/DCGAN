8afe38db3106aeecbb8bf3ac2d2fb4df6a859973
TIGERCHANG123 <2462924814@qq.com>
2020-02-23 19:04:54 +0800

    DCGAN

--- Train.py
+++ Train.py
@@ -20,13 +20,15 @@ class train_one_epoch():
         self.gen_loss, self.disc_loss = metrics
         self.train_dataset = train_dataset
         self.noise_dim = noise_dim
+        self.real_loss = 0
+        self.fake_loss = 0
 
     def train_discriminator_step(self, noise, images):
         with tf.GradientTape() as disc_tape:
             generated_images = self.generator(noise, training=True)
             real_output = self.discriminator(images, training=True)
             fake_output = self.discriminator(generated_images, training=True)
-            disc_loss = discriminator_loss(real_output, fake_output)
+            disc_loss, self.real_loss, self.fake_loss = discriminator_loss(real_output, fake_output)
         self.disc_loss(disc_loss)
         # print('disc: {}, {}'.format(disc_loss, self.disc_loss.result()))
         gradients_of_discriminator = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
@@ -35,23 +37,21 @@ class train_one_epoch():
         with tf.GradientTape() as gen_tape:
             generated_images = self.generator(noise, training=True)
             fake_output = self.discriminator(generated_images, training=True)
-            gen_loss, real_loss, fake_loss = generator_loss(fake_output)
+            gen_loss = generator_loss(fake_output)
         self.gen_loss(gen_loss)
         # print('gen: {}, {}'.format(gen_loss, self.gen_loss.result()))
         gradients_of_generator = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
         self.generator_optimizer.apply_gradients(zip(gradients_of_generator, self.generator.trainable_variables))
-        return real_loss, fake_loss
+
     def train(self, epoch,  pic):
         self.gen_loss.reset_states()
         self.disc_loss.reset_states()
         k = 0
-        real_loss = 0
-        fake_loss = 0
         for (batch, images) in enumerate(self.train_dataset):
-            if k < 3:
+            if k < 2:
                 k = k + 1
                 noise = tf.random.normal([images.shape[0], self.noise_dim])
-                real_loss, fake_loss = self.train_generator_step(noise)
+                self.train_generator_step(noise)
             else:
                 k = 0
                 noise = tf.random.normal([images.shape[0], self.noise_dim])
@@ -59,4 +59,5 @@ class train_one_epoch():
                 pic.add([self.gen_loss.result().numpy(), self.disc_loss.result().numpy()])
                 pic.save()
             if (batch + 1) % 100 == 0:
-                print('epoch: {}, gen loss: {}, disc loss: {}, real loss: {}, fake loss{}'.format(epoch, self.gen_loss.result(), self.disc_loss.result(), real_loss, fake_loss))
+                print('epoch: {}, gen loss: {}, disc loss: {}, real loss: {}, fake loss{}'
+                      .format(epoch, self.gen_loss.result(), self.disc_loss.result(), self.real_loss, self.fake_loss))
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from DCGAN import get_gan
 from show_pic import draw
 from Train import train_one_epoch
-from cartoon_face import face_dataset
+from oxford_102_64 import oxford_102_flowers_dataset
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 
@@ -19,7 +19,7 @@ def main(continue_train, train_time):
     batch_size = 64
 
     generator_model, discriminator_model, model_name = get_gan(noise_shape=100)
-    dataset = face_dataset(root, batch_size)
+    dataset = oxford_102_flowers_dataset(root, batch_size)
     model_dataset = model_name + '-' + dataset.name
 
     train_dataset = dataset.get_train_dataset()
